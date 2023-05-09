681aca9d94f7a6a5b1ac7206332567ec923f2f55
Guilherme Malosti Fonseca <gmathos77@gmail.com>
2023-05-09 20:33:39 -0300

    aula 11 condicional if

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 42.3
+
+if(vel > 50.0){
+    console.log(`Sua velocidade é de ${vel} km/h, está acima da velocidade permitida!`)
+}else{
+    console.log(`Sua velocidade é de ${vel}, está dentro da velocidade permitida!`)
+}
+console.log('Dirija com cuidado!')
---
+++ Aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'França'
+
+console.log(`Você está em ${pais}`)
+
+if(pais != 'Brasil'){
+    console.log('Você é estrangeiro')
+}else{
+    console.log('Você é brasileiro')
+}
