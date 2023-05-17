9b31a24e1cd35c38f991df176c7b4295796ed6b2
Guilherme Malosti Fonseca <gmathos77@gmail.com>
2023-05-16 21:36:12 -0300

    intriducao a variaveis compostas

---
+++ Aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5,4,2]
+num[3] = 7
+num[4] = 5
+
+for(var c = 0; c < num.length; c++){
+    console.log(num[c])
+}
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,13 @@
+
+let num = [8,1,5,3,6,8]
+/*
+for(var c = 0; c < num.length; c++){
+    console.log(num[c])
+}
+*/
+
+console.log(`Buscando valor na posição 3: ${num.indexOf(8)}`)
+
+for(let pos in num){
+    console.log(num[pos])
+}
